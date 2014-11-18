703117b35497d4df1519439a3d6a0ad76c792b4c
Alexander Cobleigh <angellus.falsus@gmail.com>
2014-11-18 22:33:48 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+/**
+ * Module dependencies.
+ */
+var express = require('express');
+var http = require('http');
+var app = express();
+var debug = false;
+
+
+// all environments
+app.set('port', '9999');
+app.use(express.logger('dev'));
+app.use(express.json());
+
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+
+if ('development' == app.get('env')) {
+    app.use(express.errorHandler());
+}
+
+// TODO: add https://piratrad.io/random when it has launched
+humerousRedirects = ["http://www.asciiartfarts.com/random.cgi", "http://wikihow.com/Special:Randomizer",
+                    "http://en.wikipedia.org/wiki/Special:Random", "http://randomcolour.com/",
+                    "http://wordsmith.org/words/random.cgi", "http://www.randomhaiku.com/",
+                    "http://www.reddit.com/r/random", "http://poetryoutloud.org/poems-and-performance/random-poem"];
+
+app.get("*", function(req, res) {
+    res.redirect(humerousRedirects[Math.floor(Math.random() * humerousRedirects.length)]);
+});
+var server = http.createServer(app);
+server.listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+});
+
+function log(msg) {
+    if (debug) {
+        console.log("SRV: " + msg);
+    }
+}
